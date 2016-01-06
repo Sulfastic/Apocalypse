1ae897ad5b226e857439e2151098d4236b5647d6
sulf <epempeyo@gmail.com>
2016-01-06 19:52:14 +0100

    Initial project configuration

---
+++ desktop/src/com/sulf/apo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sulf.apo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sulf.apo.game.Apoc;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Apoc(), config);
+	}
+}
---
+++ html/src/com/sulf/apo/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sulf.apo.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sulf.apo.game.Apoc;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Apoc();
+        }
+}
